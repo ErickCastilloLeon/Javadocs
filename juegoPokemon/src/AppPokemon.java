2b60d9da2ce8f0840c1084712effa925a7d9e72e
ecatillleo <ecatillleo@educacion.navarra.es>
2024-11-28 21:04:22 +0100

    Clase Main
    E añadido el primer comentario al Main

--- juegoPokemon/src/AppPokemon.java
+++ juegoPokemon/src/AppPokemon.java
@@ -1,6 +1,11 @@
 public class AppPokemon {
 
     public static void main(String[] args) {
+        /**
+         *Este es el Main , cuyo objetivo es ejecutar programa
+         *
+         * @Author Erick Jair Castillo Leon
+         */
         InterfazPokemon juegoPokemon = new InterfazPokemon();
         juegoPokemon.Juego();
 
